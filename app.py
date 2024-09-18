9263c679f3476770b97b993068c3978ef526223a
Cathyyu10 <cathyy2@nycstudents.net>
2024-09-18 10:13:43 -0400

    t.done

--- app.py
+++ app.py
@@ -20,6 +20,28 @@ print(z)
 """ sentence = input("Write_a_sentence: ") 
 print(len(sentence.split())) """
 
-Food = input("Food: ")
-Name = input("Name: ")
-Adjective = input("Adjective: ")
+""" noun = input("noun: ")
+number = input("number: ")
+verb = input("verb: ")
+celebrity = input("celebrity: ")
+adjective = input("adjective: ")
+adjective2 = input("adjective2: ")
+number2 = input("one_digit_number: ")
+result = input("winner_or_loser: ")
+verb2 = input("verb: ")
+emotion = input("emotion: ")
+
+print(f"Today was the {noun} competition and there were {number} other contestants. However, you were late to the competition so you decided to {verb} there. When you got there you were confronted by {celebrity} who turns out to be the judge! It was time to present your {noun}. My {noun} is very {adjective} and {adjective2}, you said. At the end of the competition you were announced as the #{number2} {result}. As a result, you decided to {verb2} {celebrity}. At the end of the day, you were {emotion}.") """
+
+""" day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else: 
+    print("incorrect") """ 
+
+x = "test"
+print(f"hello {x}")
+
+
+
+
